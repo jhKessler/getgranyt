bb43616d356030355076fb106285aba934245fbb
Johnny Kessler <68814636+jhKessler@users.noreply.github.com>
2026-01-05 14:37:46 +0100

    fix install command

--- granyt-app/src/lib/constants.ts
+++ granyt-app/src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const INSTALL_COMMAND = "curl -fsSL https://granyt.sh/install.sh | sh"
+export const INSTALL_COMMAND = "curl -fsSL https://granyt.dev/install.sh | sh"
 export const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/jhKessler/getgranyt"
